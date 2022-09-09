bdba48e84f6868caeb513d50fcee178e48eb5d01
illia <illiadimura@gmail.com>
2022-09-09 22:20:15 +0300

    ауе го флудилка

--- server/app/internal/adapters/db/mongodb/item/storage.go
+++ server/app/internal/adapters/db/mongodb/item/storage.go
@@ -1,8 +1,17 @@
 package item
 
 import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"server/app/internal/adapters/db/mongodb/item/model"
+	"server/app/internal/domain/entity"
 	"server/app/internal/domain/service"
+	"server/app/pkg/errors"
+	"time"
 )
 
 type itemStorage struct {
@@ -11,4 +20,85 @@ type itemStorage struct {
 
 func NewItemStorage(collection *mongo.Collection) service.ItemStorage {
 	return &itemStorage{collection: collection}
+}
+
+func (i itemStorage) GetItem(ctx context.Context, id string) (entity.Item, error) {
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return entity.Item{}, fmt.Errorf("parse id: %w", err)
+	}
+
+	result := i.collection.FindOne(ctx, bson.D{
+		{Key: "_id", Value: objectID},
+	})
+
+	if err = result.Err(); err != nil {
+		if err == mongo.ErrNoDocuments {
+			return entity.Item{}, entity.ErrItemNotFound
+		}
+		return entity.Item{}, errors.NewInternal("collection.FindOne", err)
+	}
+
+	var item model.Item
+	if err = result.Decode(&item); err != nil {
+		return entity.Item{}, errors.NewInternal("decode item", err)
+	}
+
+	return item.ToEntity(), nil
+}
+
+func (i itemStorage) UpdateItem(ctx context.Context, id string, item entity.Item) (created bool, err error) {
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return false, fmt.Errorf("parse id: %w", err)
+	}
+
+	opts := options.Update().SetUpsert(true)
+
+	result, err := i.collection.UpdateByID(ctx, objectID, bson.D{
+		{Key: "$set", Value: model.EntityToItem(item)},
+	}, opts)
+
+	if err != nil {
+		return false, errors.NewInternal("collection.UpdateByID", err)
+	}
+
+	return result.UpsertedCount == 1, nil
+}
+
+func (i itemStorage) DeleteItem(ctx context.Context, id string) (deleted bool, err error) {
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return false, fmt.Errorf("parse id: %w", err)
+	}
+
+	result, err := i.collection.DeleteOne(ctx, bson.D{
+		{Key: "_id", Value: objectID},
+	})
+
+	if err != nil {
+		return false, errors.NewInternal("collection.DeleteOne", err)
+	}
+
+	return result.DeletedCount == 1, nil
+}
+
+func (i itemStorage) CreateItem(ctx context.Context, name string, data []byte, description string) (string, error) {
+
+	res, err := i.collection.InsertOne(ctx, model.Item{
+		Name: name,
+
+		Data: primitive.Binary{
+			Data: data,
+		},
+
+		Created:     primitive.NewDateTimeFromTime(time.Now()),
+		Description: description,
+	})
+
+	if err != nil {
+		return "", errors.NewInternal("collection.InsertOne", err)
+	}
+
+	return res.InsertedID.(primitive.ObjectID).Hex(), nil
 }
